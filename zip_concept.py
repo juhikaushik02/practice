e98d777c06dd8a6e33aa11fb6a12b4679dc369b7
juhikaushik02 <38189801+juhikaushik02@users.noreply.github.com>
2022-03-02 23:10:39 +0530

    updated zip concept file.

--- zip_concept.py
+++ zip_concept.py
@@ -3,14 +3,11 @@ list2 = ["98789", "7654", "1543", "8976"]
 
 dict1 = {1: 'juhi', 2: 'kaushik', 3: 'abc'}
 dict2 = {1: 123, 2: 456, 3: 789}
-dict3= {123:'juhi',456:'kaushik',789:'abc'}
+dict3 = {123: 'juhi', 456: 'kaushik', 789: 'abc'}
 
-
-for (k1,v1), (k2,v2) in zip(dict1.items(),dict2.items()):
+for (k1, v1), (k2, v2) in zip(dict1.items(), dict2.items()):
     print(f' {v2} : {v1}')
     dict3[v2] = v1
-
-
 
 # print(dict3)
 
@@ -22,5 +19,4 @@ for (k1,v1), (k2,v2) in zip(dict1.items(),dict2.items()):
 for list1_item, list2_item in zip(list1, list2):
     print(list1_item + " : " + list2_item)
 
-dict4 = dict(zip(list2,list1))
-
+dict4 = dict(zip(list2, list1))
